5cfd0ca3d9e09eed088ebb4fafc138b9f3194d89
Siddharth Sharma <sidd2895@gmail.com>
2023-03-04 13:11:12 +0530

    created top and bottom lines

--- src/App.js
+++ src/App.js
@@ -4,20 +4,26 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <div
+        style={{
+          display: "flex",
+          justifyContent: "space-between",
+          alignItems: "center"
+        }}
+      >
+        <h1>People Data</h1>
+        <button
+          className="button"
         >
-          Learn React
-        </a>
-      </header>
+          Next Person
+        </button>
+      </div>
+
+      <div style={{ textAlign: "center", color: "white" }}>
+        <span>
+          Currently 3 people showing
+        </span>
+      </div>
     </div>
   );
 }
