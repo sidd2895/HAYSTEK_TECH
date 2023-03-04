a9f5a68a8c2bc1aaedd0bb89ca2c0a22dd5cefe1
Siddharth Sharma <sidd2895@gmail.com>
2023-03-04 13:42:14 +0530

    created the list to populate with css

--- src/App.js
+++ src/App.js
@@ -1,7 +1,38 @@
-import logo from './logo.svg';
 import './App.css';
+import personData from "./data.json";
 
 function App() {
+
+  const personItem = (item, index) => {
+    return (
+      <div style={{ display: "flex", marginBottom: "0.5rem" }}>
+        <div className="index">
+          <span>{index + 1}</span>
+        </div>
+        <div className="person">
+          <span
+            style={{
+              padding: "0.5rem",
+              backgroundColor: "lightblue",
+              borderRadius: "0 0.5rem 0 0"
+            }}
+          >
+            <b>Name: </b>
+            {item.name}
+          </span>
+          <span
+            style={{
+              padding: "0.5rem"
+            }}
+          >
+            <b>Location: </b>
+            {item.location}
+          </span>
+        </div>
+      </div>
+    );
+  };
+
   return (
     <div className="App">
       <div
@@ -18,6 +49,14 @@ function App() {
           Next Person
         </button>
       </div>
+
+      {personData.map((item, index) => {
+        return (
+          <div key={`${item.name}` - `${item.location}`}>
+            {personItem(item, index)}
+          </div>
+        );
+      })}
 
       <div style={{ textAlign: "center", color: "white" }}>
         <span>
